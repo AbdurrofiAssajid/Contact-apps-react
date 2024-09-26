1a29a2afd03e2db01589c711b14e57861c3a0407
Abdurrofi'i Assajid Arbya <abdurrofii.aas@gmail.com>
2024-09-27 01:11:07 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.BASE_URL": JSON.stringify(env.BASE_URL),
+      "process.env.BEARER_TOKEN": JSON.stringify(env.BEARER_TOKEN),
+    },
+    plugins: [react()],
+  };
+});

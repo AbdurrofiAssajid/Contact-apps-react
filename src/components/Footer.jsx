0e85f6a2001fe6c2ad77b3943c7484aedb36a56f
Abdurrofi'i Assajid Arbya <abdurrofii.aas@gmail.com>
2024-10-05 12:00:14 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 mt-10 px-4 text-center text-gray-500">
+    <footer>
       <small className="footer__visit">
         &copy; Bootcamp Day-3 | Abdurrofi | All rights reserved.
       </small>
-      <p className="text-xs italic font-bold">
-        <a href="https://word-analytics-site.vercel.app"
-          target="_blank">
-          Try this cool website! and try to write "rofi"</a>
-      </p>
     </footer>
   );
 }

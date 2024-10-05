67edae2156bae00358e097572ef83c56ab8f650e
Abdurrofi'i Assajid Arbya <abdurrofii.aas@gmail.com>
2024-10-06 01:03:07 +0700

    reconfigure the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
-import Footer from './components/Footer'
-import ContactInput from './containers/ContactInput'
-import ContactList from './containers/ContactList'
+import "./App.css";
+import Footer from "./components/Footer";
+import ContactInput from "./containers/ContactInput";
+import ContactList from "./containers/ContactList";
 
 function App() {
   return (
-    <main className='main__container'> 
-      <h1 className='main__container-heading'>Contact Apps</h1>
-      <ContactInput/>
-      <ContactList/>
-      <Footer/>
+    <main className="main__container">
+      <h1 className="main__container-heading">Contact Apps</h1>
+      <ContactInput />
+      <ContactList />
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
